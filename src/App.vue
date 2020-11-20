e8efbb69fe2f1952ec12c351fd9dca78e5065917
e <>
2020-11-20 17:23:03 +0800

    first conmmit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <global-heager :user="currrentUser"></global-heager>
+    <column-list :list="testData"></column-list>
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import columnList, { ColumnProps } from "./components/column-list.vue";
+import globalHeager, { UserProps } from "./components/global-header.vue";
+import validateInput, { RulesProp } from "./components/validate-input.vue";
+
+const currrentUser: UserProps = {
+  isLogin: true,
+  name: "hq"
+};
+
+const emailRules: RulesProp = [
+  { type: "required", message: "电子邮箱地址不能为空" },
+  { type: "email", message: "请输入正确的电子邮箱格式" }
+];
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是的test3专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是的test4专栏，有一段非常有意思的简介，可以更新一下欧"
+  },
+  {
+    id: 5,
+    title: "test5的专栏",
+    description: "这是的test5专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  }
+];
+
+export default defineComponent({
+  name: "App",
+  components: {
+    columnList,
+    globalHeager,
+    validateInput
+  },
+  setup() {
+    return {
+      testData,
+      currrentUser,
+      emailRules
+    };
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/dropdownItem .vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="dropdown-option" :class="{ 'is-disabled': status }">
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+export default defineComponent({
+  name: "DropdownItem",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, ctx) {
+    const status = computed(() => {
+      console.log(ctx);
+      return props.disabled;
+    });
+    return {
+      status
+    };
+  }
+});
+</script>
+
+<style>
+.dropdown-option.is-disabled * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+</style>
